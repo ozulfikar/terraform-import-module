a611d55c1bd7611909ac3deddf7802384b73a160
ozulfikar <97890218+ozulfikar@users.noreply.github.com>
2022-11-09 09:42:19 +0300

    terraform-import-module-lesson3

---
+++ modules/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "module_vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "icterra-vpc-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  cidr_block = var.public_subnet_cidr
+  vpc_id = aws_vpc.module_vpc.id
+  tags = {
+    Name = "public-subnet-${var.environment}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  cidr_block = var.private_subnet_cidr
+  vpc_id = aws_vpc.module_vpc.id
+  tags = {
+    Name = "private-subnet-${var.environment}"
+  }
+}
